c4fb741ce8aaccee4f94b9fba1ec9e7ecaf9859f
dorayo <yomingyo@gmail.com>
2015-08-04 09:18:31 +0800

    Initial Commit

---
+++ RemoteNotificationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RemoteNotificationDemo
+//
+//  Created by NavchinaMacBook on 15/8/4.
+//  Copyright (c) 2015年 NavchinaMacBook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RemoteNotificationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RemoteNotificationDemo
+//
+//  Created by NavchinaMacBook on 15/8/4.
+//  Copyright (c) 2015年 NavchinaMacBook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
